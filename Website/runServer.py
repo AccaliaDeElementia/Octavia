d375ddb3be16926d6fa33d1d9394c0e6c3121a58
Accalia de Elementia <accalia.de.elementia@gmail.com>
2012-06-12 21:56:15 -0400

    Clear old versions. Begin redesign as RPC/WebSockets design.

--- Website/runServer.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python2
-
-from Octavia import app
-if __name__ == '__main__':
-    app.run(port=5001)
--- python3/runService.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-import Octavia
-from bottle import run
-
-run(Octavia.app, host='localhost', port=8000, reloader=True)
