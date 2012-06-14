5a5149616a1d7369a8a940cf5ebeac8e88e51fb1
Accalia de Elementia <accalia.de.elementia@gmail.com>
2012-06-14 08:33:30 -0400

    adjust library stack.
    Using Bottle
    Bottle-websockets (patched to pythion3)
    jsonrpc2 (patched to python3)
    wsgi-xmlrpc (patched to python3)

--- Octavia/setup_virtualenv.sh
+++ Octavia/setup_virtualenv.sh
@@ -1,6 +1,21 @@
-virtualenv2 .
+#!/bin/bash
+function fix2to3 {
+    tree -inf "$1"|egrep py$|while read i; do 2to3 -w $i; done
+}
+
+virtualenv .
 source bin/activate
-pip install gevent-websocket
-pip install flask
-pip install Flask-XML-RPC
+
+pip install bottle
+
 pip install jsonrpc2
+fix2to3 "lib/python3.2/site-packages/jsonrpc2"
+
+pip install wsgi-xmlrpc
+fix2to3 "lib/python3.2/site-packages/wsgi-xmlrpc"
+
+pip install bottle-websocket
+touch "build/bottle-websocket/README.md"
+pip install bottle-websocket
+fix2to3 "build/gevent"
+pip install bottle-websocket
