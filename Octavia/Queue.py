a86cd6f144e27428bdc2242bdcc614177038d568
Accalia de Elementia <accalia.de.elementia@gmail.com>
2012-06-18 07:59:49 -0400

    Update Queue.py, closing multiple issues on github

--- Octavia/Queue.py
+++ Octavia/Queue.py
@@ -12,3 +12,25 @@ def queueList(sid=None):
 @octavia.register
 def queueCurrent():
     return filter_song(mpc.client.currentsong())
+
+@octavia.register
+def queueNext():
+    return mpc.client.next()
+
+@octavia.register
+def queuePrev():
+    return mpc.client.previous()
+
+@octavia.register
+def queuePlay(sid=None)
+    if sid is None:
+        return mpc.client.play()
+    return mpd.client.play(sid)
+
+@octavia.register
+def queuePause():
+    return mpc.client.pause(1)
+
+@octavia.register
+def queueStop():
+    return mpc.client.stop()
